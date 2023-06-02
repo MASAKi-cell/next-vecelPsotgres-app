658c4a068fab33d78951dea12a4f49f5b2571146
masaki takahashi <bje35149@gmail.com>
2023-06-02 20:10:57 +0900

    main画面 レイアウト修正

--- pages/top/main.tsx
+++ pages/top/main.tsx
@@ -2,8 +2,41 @@ import styles from "style/styles.module.css";
 
 export default function TopPage() {
   return (
-    <main className={styles.BackgroundPaper}>
-      <div className="">Login Success!!</div>
-    </main>
+    <div>
+      <header className={styles.header}>
+        <div className={styles.headerParent}>
+          <div className={styles.textTitle}>
+            <h1 className={styles.textGradient}>Github Auth</h1>
+          </div>
+          <div className={styles.row}>
+            <nav className={styles.headerNav}>
+              <ul>
+                <li className={styles.current}>
+                  <a className="smoothscroll" href="">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a className="smoothscroll" href="">
+                    Profile
+                  </a>
+                </li>
+                <li>
+                  <a className="smoothscroll" href="">
+                    About me
+                  </a>
+                </li>
+                <li>
+                  <a className="smoothscroll" href="">
+                    Logout
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+      <main className={styles.BackgroundPaper}></main>
+    </div>
   );
 }
